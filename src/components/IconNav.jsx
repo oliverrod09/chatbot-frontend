2b036a70c71ed3aa0817768273f8db04746180c3
Oliverrod09 <oliver.antionio.09@gmail.com>
2023-07-21 15:09:53 -0600

    ultimo push antes de la presentacion

--- src/components/IconNav.jsx
+++ src/components/IconNav.jsx
@@ -16,6 +16,7 @@ import {
 import { Link, Navigate } from "react-router-dom";
 
 export default function IconNav({sesion}) {
+  const userImage = "/svg/user.svg"
   return (
     <Menu className="">
       <MenuHandler>
@@ -23,7 +24,7 @@ export default function IconNav({sesion}) {
           variant="circular"
           alt="tania andrew"
           className="cursor-pointer"
-          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+          src={sesion ? "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" : userImage}
         />
       </MenuHandler>
       <MenuList className="">
